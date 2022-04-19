d6ca04e0455bbab255ae0c6d15b5e60735aef663
Smarsden007 <seannmarsden@gmail.com>
2022-04-18 22:01:45 -0700

    addOne state added

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,16 @@ import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
 import reducer, { initialState } from '../reducers'
+import { addOne } from '../actions'
+
+
 
 function App() {
   const [ state, dispatch ] = useReducer(reducer, initialState)
+
+  const handle1Click = () =>{
+    dispatch(addOne());
+  }
 
   return (
     <div className="App">
@@ -20,7 +27,7 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={state.total}}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
               <span id="operation"><b>Operation:</b> {state.operation}</span>
               <span id="memory"><b>Memory:</b> {state.memory}</span>
@@ -33,7 +40,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handle1Click}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
 
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 420,
+    operation: "+",
+    memory: 5
 }
 
 const calculateResult = (num1, num2, operation) => {
